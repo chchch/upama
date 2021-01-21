6068a61aa7e3572af5dd5005e562625383662e72
chchch <cchl2@cam.ac.uk>
2021-01-21 19:14:33 +0100

    added style updates

--- dokuwiki/lib/tpl/saktumiva/tpl_header.php
+++ dokuwiki/lib/tpl/saktumiva/tpl_header.php
@@ -11,28 +11,6 @@ if (!defined('DOKU_INC')) die();
 <div id="dokuwiki__header"><div class="pad group">
 
     <?php tpl_includeFile('header.html') ?>
-<!--
-    <div class="headings group">
-        <ul class="a11y skip">
-            <li><a href="#dokuwiki__content"><?php echo $lang['skip_to_content']; ?></a></li>
-        </ul>
-
-        <h1><?php
-            // get logo either out of the template images folder or data/media folder
-            $logoSize = array();
-            $logo = tpl_getMediaFile(array(':wiki:logo.png', ':logo.png', 'images/logo.png'), false, $logoSize);
-
-            // display logo and wiki title in a link to the home page
-            tpl_link(
-                wl(),
-                '<img src="'.$logo.'" '.$logoSize[3].' alt="" /> <span>'.$conf['title'].'</span>',
-                'accesskey="h" title="[H]"'
-            );
-        ?></h1>
-        <?php if ($conf['tagline']): ?>
-            <p class="claim"><?php echo $conf['tagline']; ?></p>
-        <?php endif ?>
-    </div> -->
 
     <div class="tools group">
         <!-- USER TOOLS -->
